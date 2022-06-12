4c7bdedf23a2f790777a352f095095bb61b5a27c
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-06-12 17:27:40 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,6 +17,16 @@ using std::string;  using std::vector;
 
 #include  <iterator>
 
+struct {
+    const string cyan = "\033[36m";
+    const string magenta = "\033[35m";
+    const string red = "\033[91m";
+    const string gray = "\033[8m";
+    const string green = "\033[92m";
+    const string yellow = "\033[33m";
+    const string end = "\033[0m";
+} colors;
+
 // randomization code here is copied from stackoverflow
 template<typename Iter, typename RandomGenerator>
 Iter select_randomly(Iter start, Iter end, RandomGenerator& g) {
@@ -33,7 +43,7 @@ Iter select_randomly(Iter start, Iter end) {
 }
 
 void cls() {
-    //system("clear");
+    system("clear");
 }
 
 void end_game() {
@@ -45,8 +55,10 @@ void end_game() {
 
 class card {
 public:
-    card (string n, string d, string ef, int c, int r) {
+    card (string n, string d, string ef, int c, int r, string cl, int tp) {
         name = n;
+        type = tp;
+        color = cl;
         rarity = r;
         desc = d;
         cost = c;
@@ -54,7 +66,9 @@ public:
     }
 
     int rarity; // 0 common, 1 uncommon, 2 rare, 3 very rare, 4 - upgraded cards
+    int type; // 0 attack, 1 skill
     int cost;
+    string color;
     string name;
     string desc;
     char effect[EFFECT_LENGTH];
@@ -73,16 +87,19 @@ class player {
 public:
     string name;
     int drawcards = 4;
-    int drawlimit = 10;
+    int drawlimit = 10; // how many cards can be in hand at once
     int barricade = 0; // don't lose block for x turns
+    int gold = 0;
+    int level = 0; // each act has a number of levels, stored here
     int blockdiscard = 0; // don't discard hand for x turns
+    int act = 0; // game is split into 3 acts, this stores the act number
     int poison = 0;
     int frail = 0; // weaken block cards
     int maxmana = 3;
     int mana;
     int weak; // -1 weak each turn
-    int hp = 25;
-    int maxhp = 25;
+    int hp = 50;
+    int maxhp = 50;
     int block = 0;
     int strength = 0;
 
@@ -110,7 +127,7 @@ public:
 };
 
 class enemy;
-char eval_effect(char effect[EFFECT_LENGTH], player* plr, enemy* enemy, pile* pl_pile);
+void eval_effect(char effect[EFFECT_LENGTH], player* plr, enemy* enemy, pile* pl_pile);
 class enemy {
 public:
     string name;
@@ -146,6 +163,13 @@ public:
         }
         else
             block -= rdmg;
+    }
+
+    bool check_hp() {
+        if (hp < 1) {
+            return false;;
+        }
+        return true;
     }
 
     void addblock(int blc) {
@@ -201,6 +225,10 @@ int select_from_hand(pile* plc, string msg) {
     return (inp - '0' - 1);
 }
 
+void buffer_flush() {
+    msgbuffer = "";
+}
+void buffer_queue();
 // Evaluate effect of card
 // d = dmg
 // p = pois
@@ -210,17 +238,22 @@ int select_from_hand(pile* plc, string msg) {
 // [b]lock, [d]amage enemy, dis[c]ard other card(s), [p]oison
 // [h]eal, e[x]haust other card(s), [H]eal enemy, [D]amage player
 // [B]lock enemy
-char eval_effect(char effect[EFFECT_LENGTH], player* plr, enemy* en, pile* pl_pile) {
+void eval_effect(char effect[EFFECT_LENGTH], player* plr, enemy* en, pile* pl_pile) {
+    buffer_flush();
     int tmpnum = 0;
-    int mx;
     for (int i = 0; i < EFFECT_LENGTH-1; i++) {
         if (effect[i] == '\0') break;
+
         if (isdigit(effect[i])) tmpnum += (effect[i] - '0');
         else {
-            if (effect[i] == 'd') { en->damage(tmpnum, plr->strength); tmpnum = 0; }
-            else if (effect[i] == 'D') { plr->damage(tmpnum, en->strength); tmpnum = 0; }
-            else if (effect[i] == 'b') { plr->addblock(tmpnum); tmpnum = 0; }
-            else if (effect[i] == 'B') { en->addblock(tmpnum); tmpnum = 0; }
+            if (effect[i] == 'd') { en->damage(tmpnum, en->strength);
+                buffer_queue(colors.red+"You hit for "+std::to_string(tmpnum)+" damage"+colors.end); tmpnum = 0; }
+            else if (effect[i] == 'D') { plr->damage(tmpnum, en->strength);
+                buffer_queue(colors.red+"You take "+std::to_string(tmpnum)+" damage"+colors.end); tmpnum = 0; }
+            else if (effect[i] == 'b') { plr->addblock(tmpnum );
+                buffer_queue(colors.cyan+"You gain "+std::to_string(tmpnum)+" block"+colors.end); tmpnum = 0; }
+            else if (effect[i] == 'B') { en->addblock(tmpnum );
+                buffer_queue(colors.cyan+"Enemy gains "+std::to_string(tmpnum)+" block"+colors.end); tmpnum = 0; }
             else if (effect[i] == 'c') {
                 for (int i = 0; i < tmpnum; i++) {
                     if (pl_pile->hand.size() > 0) {
@@ -232,10 +265,8 @@ char eval_effect(char effect[EFFECT_LENGTH], player* plr, enemy* en, pile* pl_pi
                 tmpnum = 0;
             }
         }
-        mx = i;
     }
 
-    return effect[mx];
 }
 
 // convert effect number to int (991 to 19 etc.)
@@ -318,14 +349,24 @@ void discard_hand(player* pl, pile* pl_cards) {
 
 void print_game(player* pl, pile* pl_cards, enemy* en) {
     cls();
-    cout << pl->name << " the ironclad\t\t\t\t\t" << en->name << std::endl;
-    cout << "HP: " << pl->hp << "\t\t\t\t\t\t\tEnemy HP: " << en->hp << "\n";
-    cout << "Block: " << pl->block << "\t\t\t\t\t\tEnemy block: " << en->block << "\n";
-    cout << "Mana: " << pl->mana << "\t\t\t\t\t\t\tEnemy intent: " << enemy_intention_to_string(en->intention) << "\n";
+    cout << colors.green << "Act: " << pl->act+1 << "/3" << "\t\t\t" << "Level: " << pl->level << colors.red
+         << "\t\t\tDeck: " << pl_cards->deck.size() << " cards";
+    cout << colors.yellow << "\t\t\tGold: " << pl->gold << colors.end << "\n";
+    cout << string(111, '-') << "\n";
+    cout << colors.yellow << pl->name << " the ironclad\t\t\t\t\t\t\t\t\t\t" << en->name << colors.end << std::endl;
+    cout << "HP: " << colors.green << pl->hp << colors.end << "\t\t\t\t\t\t\t\t\t\t\t\tEnemy HP: " << colors.green << en->hp << colors.end << "\n";
+    cout << "Block: " << colors.cyan << pl->block << colors.end << "\t\t\t\t\t\t\t\t\t\t\tEnemy block: " << colors.cyan << en->block << colors.end << "\n";
+    cout << "Mana: " << colors.magenta << pl->mana << colors.end << "\t\t\t\t\t\t\t\t\t\t\t\tEnemy intent: " << colors.magenta << enemy_intention_to_string(en->intention) << colors.end << "\n";
+    cout << string(111, '-') << "\n";
+    string mydesc;
     for (unsigned long int i = 0; i < pl_cards->hand.size(); i++) {
-        cout << "(" << i+1 << ") " << pl_cards->hand.at(i).name << "\t\t:: " << pl_cards->hand.at(i).desc
-             << "  \t\t:: Mana cost: " << pl_cards->hand.at(i).cost << std::endl;
+        mydesc = pl_cards->hand.at(i).desc;
+        while (mydesc.length() < 60) mydesc += " ";
+        cout << pl_cards->hand.at(i).color << "(" << i+1 << ") " << pl_cards->hand.at(i).name << colors.end << "\t\t\t:: "
+             << mydesc << colors.magenta
+             << "\t" << ":: Mana cost: " << pl_cards->hand.at(i).cost << colors.end << std::endl;
     }
+    cout << "\n\n" << msgbuffer << "\n";
 }
 
 
@@ -345,10 +386,13 @@ void start_turn(player* pl, pile* pl_cards) {
     pl->mana = pl->maxmana; // refresh mana
 }
 
-char eval_card(player* pl, pile* pl_pile, enemy* en, card crd) {
+void eval_card(player* pl, pile* pl_pile, enemy* en, card crd) {
     pl->mana-=crd.cost;
     print_game(pl, pl_pile, en);
-    return eval_effect(crd.effect, pl, en, pl_pile);
+    char tmpef[EFFECT_LENGTH];
+    strcpy(tmpef, crd.effect);
+    tmpef[strlen(tmpef)-1] = '\0';
+    eval_effect(tmpef, pl, en, pl_pile);
 }
 
 // discard x card from hand
@@ -373,6 +417,7 @@ void play_card_from_hand(player* pl, pile* pl_cards, enemy* en, int index) {
         eval_card(pl, pl_cards, en, cr);
     }
     else {
+        buffer_flush();
         buffer_queue("Not enough mana to play card.");
     }
 }
@@ -389,6 +434,11 @@ void create_fight(player* pl, pile* plc, enemy* en_main) {
             cin >> choice;
             if (choice > '0' && choice < (char)('1' + plc->hand.size()))
                 play_card_from_hand(pl, plc, en_main, (int)(choice - '0')-1);
+            if (!en_main->check_hp()) {
+                cout << colors.red << "You kill the " << en_main->name << "!\n" << colors.end;
+                fight = false;
+                break;
+            }
             print_game(pl, plc, en_main);
         }
         en_main->start_turn();
@@ -406,15 +456,17 @@ void init_game(vector<enemy>* env, vector<card>* crds) {
     /* Define cards TODO: read from a database file */
     // TODO: define card pairings for upgraded variants
     //       possibly use a vector which will point to each card's variants?
-    crds->push_back(card("Strike", "Deal 6 damage","6dD",1,0)); // before last value is mana cost, last is rarity
-    crds->push_back(card("Defend", "Get 5 block","5bD",1,0));
-    crds->push_back(card("Iron mask", "Get 10 block and discard another card", "91b1cD",1,0));
-    crds->push_back(card("Strike+", "Deal 9 damage","9dD",1,4));
-    crds->push_back(card("Defend+", "Get 8 block","8bD",1,4));
-    crds->push_back(card("Iron mask+", "Get 13 block and discard another card", "94b1cD",1,4));
+    //       name - desc - mana - rarity - color - type (0 attack, 1 skill)
+    crds->push_back(card("Strike", "Deal 6 damage","6dD",1,0, colors.red,0));
+    crds->push_back(card("Defend", "Get 5 block","5bD",1,0, colors.cyan,1));
+    crds->push_back(card("Iron mask", "Get 10 block and discard another card", "91b1cD",1,0, colors.cyan,1));
+    crds->push_back(card("Strike+", "Deal 9 damage","9dD",1,4,colors.red,0));
+    crds->push_back(card("Defend+", "Get 8 block","8bD",1,4,colors.cyan,1));
+    crds->push_back(card("Iron mask+", "Get 13 block and discard another card", "94b1cD",1,4,colors.cyan,1));
 
 }
 
+// Create deck at the beginning of the game
 pile create_deck(vector<card> crds) {
     pile pl_pile;
     for (int i = 0; i < 3; i++)
@@ -425,6 +477,14 @@ pile create_deck(vector<card> crds) {
     return pl_pile;
 }
 
+// Create player at the beginning of the game
+player create_player() {
+    player pl;
+    cout << "Enter your name:\n";
+    cin >> pl.name;
+    return pl;
+}
+
 int main() {
     std::vector<enemy> enemies;
     vector<card> cards;
@@ -432,11 +492,10 @@ int main() {
     init_game(&enemies, &cards);
 
     // Initialize player and deck
-    player pl;
+    player pl = create_player();
     pile pl_pile = create_deck(cards);
     // Initialize enemy
-    enemy en_main = enemies.at(0);
-    pl.name = "Jan";
+    enemy en_main = enemies.at(0); // Goblin enemy
 
 
     create_fight(&pl, &pl_pile, &en_main);
